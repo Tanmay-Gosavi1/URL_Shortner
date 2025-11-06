b84bc1fd9f5acb6fbf4f8f40ca3f7f82959d636a
Tanmay Gosavi <202452335@iiitvadodara.ac.in>
2025-11-07 04:05:03 +0530

    fix: update width class for App component to ensure full width layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Landing from './pages/Landing'
 
 const App = () => {
   return (
-    <div className="min-h-screen w-screen">
+    <div className="min-h-screen w-full">
       <Landing />
     </div>
   )
